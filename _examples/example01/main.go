b06bbdf398b23978fd1fa8f38d72a69c5bb01737
lixin <lixin@lixins-MacBook-Pro.local>
2023-09-13 10:37:24 +0800

    update GenXid func

--- _examples/example01/main.go
+++ _examples/example01/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/binary"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lixvyang/ginzero"
@@ -17,13 +21,34 @@ func NewLogger() zerolog.Logger {
 	return logger
 }
 
+var pid = uint32(os.Getpid())
+
+func GenerateRandnum() int {
+	rand.Seed(time.Now().UnixNano())
+	randNum := rand.Intn(2 << 31)
+	return randNum
+}
+
+func defaultGenReqId() string {
+	var b [16]byte
+
+	binary.LittleEndian.PutUint32(b[:], pid)
+	binary.LittleEndian.PutUint64(b[4:], uint64(time.Now().UnixNano()))
+	binary.LittleEndian.PutUint32(b[12:], uint32(GenerateRandnum()))
+	return base64.URLEncoding.EncodeToString(b[:])
+}
+
 func main() {
 	logger := NewLogger()
 	r := gin.New()
-	r.Use(ginzero.Ginzero(&logger, ginzero.WithSkipPaths([]string{"/hello"})), ginzero.RecoveryWithZero(&logger, true))
+	r.Use(ginzero.Ginzero(&logger, ginzero.SkipPaths([]string{"/hello"}), ginzero.Genxid(defaultGenReqId)), ginzero.RecoveryWithZero(&logger, true))
 
 	r.GET("/hello", func(c *gin.Context) {
-		c.String(200, "hello")
+		c.String(200, "hello1")
+	})
+
+	r.GET("/hello/2", func(c *gin.Context) {
+		c.String(200, "hello2")
 	})
 
 	r.GET("/panic", func(c *gin.Context) {
